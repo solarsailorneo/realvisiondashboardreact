71c291ebc48dfee30e19c6e5922feb33d11ffb82
Solar Sailor <solarsailorneo@protonmail.com>
2023-09-12 16:31:08 -0400

    added first version of backend
    finished backend prototype

--- src/components/TimeSeriesDashboard.jsx
+++ src/components/TimeSeriesDashboard.jsx
@@ -1,32 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
-
-const mockData = {
-  chart1: [
-    [{ Time: '2023-01-01', Value: 100 }, { Time: '2023-01-02', Value: 105 }],
-    [{ Time: '2023-01-01', Value: 50 }, { Time: '2023-01-02', Value: 52 }]
-  ],
-  chart2: [
-    [{ Time: '2023-01-01', Value: 200 }, { Time: '2023-01-02', Value: 205 }],
-    [{ Time: '2023-01-01', Value: 75 }, { Time: '2023-01-02', Value: 78 }]
-  ],
-  chart3: [
-    [{ Time: '2023-01-01', Value: 200 }, { Time: '2023-01-02', Value: 205 }],
-    [{ Time: '2023-01-01', Value: 75 }, { Time: '2023-01-02', Value: 78 }]
-  ],
-  chart4: [
-    [{ Time: '2023-01-01', Value: 200 }, { Time: '2023-01-02', Value: 205 }],
-    [{ Time: '2023-01-01', Value: 75 }, { Time: '2023-01-02', Value: 78 }]
-  ]
-  // ... add mock data for chart3 and chart4 similarly
-};
+import axios from 'axios';
 
 const TimeSeriesDashboard = () => {
+  const [chartData, setChartData] = useState({});
+
+  useEffect(() => {
+    // Fetch data from the backend
+    axios.get('http://127.0.0.1:8080/data')
+      .then(response => {
+        setChartData(response.data);
+      })
+      .catch(error => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
+
   return (
     <div>
       <h1>Time Series Dashboard</h1>
       <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-between' }}>
-        {Object.keys(mockData).map(key => (
+        {Object.keys(chartData).map(key => (
           <div key={key} style={{ width: 'calc(50% - 10px)', marginBottom: '20px' }}>
             <h2>{key}</h2>
             <LineChart width={500} height={300}>
@@ -35,7 +29,7 @@ const TimeSeriesDashboard = () => {
               <YAxis />
               <Tooltip />
               <Legend />
-              {mockData[key].map((timeSeries, index) => (
+              {chartData[key]?.map((timeSeries, index) => (
                 <Line key={index} type="monotone" data={timeSeries} dataKey="Value" />
               ))}
             </LineChart>
